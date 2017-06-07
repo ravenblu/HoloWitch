1e4245e4db306c61da6677aeb9939d60eca95463
joylio <jiayu.dev@gmail.com>
2017-06-07 12:40:55 -0400

    the game

---
+++ Assets/Scripts/FireController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireController : MonoBehaviour {
+
+	public ParticleSystem[] ps;
+	public float rate;
+
+	public void IncreaseFire() {
+		for(int i = 0; i<ps.Length; i++) {
+			print(ps.Length);
+			var emi = ps[i].emission;
+			emi.rateOverTime =+ rate;
+		}
+	}
+
+	public void DecreaseFire() {
+		for(int i = 0; i<ps.Length; i++) {
+			var emi = ps[i].emission;
+			emi.rateOverTime =- rate;
+		}
+	}
+
+
+}
